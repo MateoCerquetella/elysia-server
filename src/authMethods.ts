8d1d5d15f9852f4d1438365b47937c3c4078c2c4
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-06-02 17:46:15 -0300

    chore: add @elysiajs/cookie and @elysiajs/jwt dependencies to package.json feat: add authMethods.ts file to handle authentication routes and methods refactor: move authMethods routes and methods to authMethods.ts file refactor: remove unused client.ts file refactor: clean up server.ts file and import authMethods from authMethods.ts chore: remove commented out code from userMethods.ts chore: clean up tsconfig.json file and remove unnecessary comments

---
+++ src/authMethods.ts
@@ -0,0 +1,58 @@
+// authMethods.ts
+import { Elysia, t } from 'elysia';
+
+import cookie from '@elysiajs/cookie';
+import { jwt } from '@elysiajs/jwt';
+import { swagger } from '@elysiajs/swagger';
+
+async function signIn({
+  username,
+  password
+}: {
+  username: string;
+  password: string;
+}) {
+  // This is a placeholder for your sign-in logic.
+  // In a real application, you would check the username and password against a database.
+  if (username === 'username' && password === 'password') {
+    return { success: true };
+  } else {
+    return { success: false };
+  }
+}
+
+export const authMethods = (app: Elysia) =>
+  app
+    .use(swagger())
+    .use(cookie())
+    .use(
+      jwt({
+        name: 'jwt',
+        secret: 'es un secreto de tu mirada y la mia'
+      })
+    )
+    .post('/sign-in', ({ body }) => signIn(body), {
+      body: t.Object({
+        username: t.String(),
+        password: t.String()
+      })
+    })
+    .get('/sign/:name', async ({ jwt, cookie, setCookie, params }) => {
+      setCookie('auth', await jwt.sign(params), {
+        httpOnly: true,
+        maxAge: 7 * 86400
+      });
+
+      return `Sign in as ${cookie.auth}`;
+    })
+    .get('/profile', async ({ jwt, set, cookie: { auth } }) => {
+      const profile = await jwt.verify(auth);
+
+      if (!profile) {
+        set.status = 401;
+        return 'Unauthorized';
+      }
+
+      return `Hello ${profile.name}`;
+    })
+    .get('/hello', () => 'Hello, world! :)');
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,8 @@
-import { Elysia, t } from 'elysia';
+import { Elysia } from 'elysia';
 
-import { swagger } from '@elysiajs/swagger';
+import { authMethods } from './authMethods';
 
-async function signIn({
-  username,
-  password
-}: {
-  username: string;
-  password: string;
-}) {
-  // This is a placeholder for your sign-in logic.
-  // In a real application, you would check the username and password against a database.
-  if (username === 'username' && password === 'password') {
-    return { success: true };
-  } else {
-    return { success: false };
-  }
-}
+const app = new Elysia();
 
-const app = new Elysia()
-  .use(swagger())
-  .post('/sign-in', ({ body }) => signIn(body), {
-    body: t.Object({
-      username: t.String(),
-      password: t.String()
-    })
-  })
-  .get('/hello', () => 'Hello, world! :)')
-  .listen(8080);
-
-export type App = typeof app;
+authMethods(app);
+app.listen(8080);
